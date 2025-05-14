2cf81243371c857950c535adadcb90d53058d433
rzvbka <wikstef2@gmail.com>
2025-05-14 18:07:41 +0200

    new gemini model

--- app/api/trip-planning/route.ts
+++ app/api/trip-planning/route.ts
@@ -5,7 +5,7 @@ export const runtime = 'edge';
 const googleAI = new GoogleGenerativeAI(
   process.env.NEXT_PUBLIC_GEMINI_API_KEY!
 );
-const MODEL_NAME = 'gemini-2.0-flash-thinking-exp-01-21';
+const MODEL_NAME = 'gemini-2.5-flash-preview-04-17';
 const PEXELS_API_KEY = process.env.PEXELS_API_KEY!;
 
 export async function POST(request: NextRequest) {
